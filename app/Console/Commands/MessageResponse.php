a6b4514f5bc593ffed7c8a6135c170b6a937f33f
NoWitam <wowek31@gmail.com>
2023-07-09 07:45:37 +0200

    a

--- app/Console/Commands/MessageResponse.php
+++ app/Console/Commands/MessageResponse.php
@@ -31,14 +31,15 @@ class MessageResponse extends Command
         //$match = Pair::find($this->argument('match'));
         $number = $this->argument('match');
         if($number == 'start') {
-            $proc1 = new Process(['php artisan app:message-response 1']);
-            $proc1->start();
-            $proc2 = new Process(['php artisan app:message-response 2']);
-            $proc2->start();
+            $proc1 = \Illuminate\Support\Facades\Process::run(['php artisan app:message-response 5']);
+           // $proc1->start();
+            $proc2 = \Illuminate\Support\Facades\Process::run(['php artisan app:message-response 6']);
+           // $proc2->start();
         } else {
             for($i=0; $i<20; $i++)
             {
                 Log::info("Task " . $number . " call " . $i);
+                sleep(1);
             }
         }
     }
